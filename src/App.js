a25b12c2393d291548fff07573638aa7d27f4fe3
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-01 20:21:26 -0300

    estado com redu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Intervalo } from './Components/Intervalo/Intervalo';
-import { Media } from './Components/Media/Media';
+import Media from './Components/Media/Media';
 import { Soma } from './Components/Soma/Soma';
 import { Sorteio } from './Components/Sorteio/Sorteio';
 
--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -1,17 +1,28 @@
+import { connect } from "react-redux";
 import { Card } from "../Card/Card";
 
-export const Media = (props) => {
-
-
+function Media (props) {
+    const {min, max} = props;
+    console.log((max + min) / 2)
+    
     return(
         <Card title={'Média dos Números'} green>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>9</strong>
+                    <strong>{(max + min) / 2}</strong>
                 </span>
             </div>
         </Card>
 
     );
 }
+
+const mapStateToProps = (state) => {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max,
+    }
+}
+
+export default connect(mapStateToProps)(Media)
